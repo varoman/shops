15d14748c474199bfb42dd6f867a2b65b65db73e
varoman <you@example.com>
2017-09-24 14:12:48 +0400

    remove unneccessary files

--- app/routes/shops/shop.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-	model(params) {
-		var shops = this.modelFor('shops');
-		console.log(params);
-		return shops.findBy('name', params.slug);
-	}
-});
